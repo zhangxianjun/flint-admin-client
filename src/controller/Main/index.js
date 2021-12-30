226734740a1f5599d029ef8fa9c0f96260238043
zhangxianjun <1581758003@qq.com>
2021-12-30 11:13:53 +0800

    feat - xxx

---
+++ src/controller/Main/index.js
@@ -0,0 +1,19 @@
+// import { Button } from 'antd';
+// import ProLayout, { PageContainer } from '@ant-design/pro-layout';
+
+// export default (
+//   <ProLayout>
+//     <PageContainer
+//       extra={[
+//         <Button key="3">Operating</Button>,
+//         <Button key="2">Operating</Button>,
+//         <Button key="1" type="primary">
+//           Main Operating
+//         </Button>,
+//       ]}
+//       footer={[<Button>reset</Button>, <Button type="primary">submit</Button>]}
+//     >
+//       {children}
+//     </PageContainer>
+//   </ProLayout>
+// );
